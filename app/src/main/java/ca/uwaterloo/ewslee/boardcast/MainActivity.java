317d3bbf711f4f44c64f0f194bf613ae557ddda8
ElginLee <ELGI0001@e.ntu.edu.sg>
2018-02-02 03:02:17 -0500

    Commit test

--- app/src/main/java/ca/uwaterloo/ewslee/boardcast/MainActivity.java
+++ app/src/main/java/ca/uwaterloo/ewslee/boardcast/MainActivity.java
@@ -1,5 +1,6 @@
 package ca.uwaterloo.ewslee.boardcast;
 
+import android.content.SyncStatusObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SyncStatusObserver();
     }
 }
